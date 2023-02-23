4f68a5c5a79ddd7c1b8d0edad3b9b51f4f39336b
Evan Tung <dragonejt@gmail.com>
2023-02-22 19:34:34 -0700

    integrate Perspective API

---
+++ actions/perspectiveAPI.js
@@ -0,0 +1,25 @@
+import { google } from "googleapis";
+
+const PERSPECTIVE_API_URL = "https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1";
+const perspectiveAPI = await google.discoverAPI(PERSPECTIVE_API_URL);
+
+export const analyzeMessage = async message => {
+    if (message.isChatInputCommand || message.guildId != "1063590532711972945") return;
+
+    try {
+        const analysis = await perspectiveAPI.comments.analyze({
+            key: process.env.PERSPECTIVE_API_KEY,
+            resource: {
+                comment: {
+                    text: message.content
+                },
+                requestedAttributes: {
+                    TOXICITY: {}
+                }
+            }
+        });
+        console.log(analysis.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
--- sibyl-discord.js
+++ sibyl-discord.js
@@ -1,41 +1,41 @@
-import { REST, Routes, Client, GatewayIntentBits, Collection } from "discord.js";
+import { REST, Routes, Client, GatewayIntentBits, Collection, Events } from "discord.js";
+
 import sibylCommand from "./commands/sibyl.js";
 import dominatorCommand from "./commands/dominator.js";
 import psychopassCommand from "./commands/psychopass.js";
 
+import { analyzeMessage } from "./actions/perspectiveAPI.js";
+
 const commands = [sibylCommand, dominatorCommand, psychopassCommand];
-
-const registerCommands = async () => {
-    try {
-        const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN);
-        console.log('Started refreshing application (/) commands.');
-        await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), { body: commands.map(command => command.data) });
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection();
-
-for (const command of commands) {
-    if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.warn("The following command is missing a data or execute attribute:");
-        console.warn(command);
-    }
-}
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setPresence({ activities: [{ name: "1 servers", type: 3 }] })
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN);
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+console.log('Started refreshing application (/) commands.');
+await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), { body: commands.map(command => command.data) });
+console.log('Successfully reloaded application (/) commands.');
 
+client.commands = new Collection();
+commands.map(command => client.commands.set(command.data.name, command));
+
+client.on(Events.ClientReady, () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ activities: [{ name: `${client.guilds.cache.size} Servers`, type: 3 }] });
+});
+
+client.on(Events.GuildCreate, () => {
+    client.user.setPresence({ activities: [{ name: `${client.guilds.cache.size} Servers`, type: 3 }] });
+})
+
+client.on(Events.MessageCreate, analyzeMessage);
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
     const command = interaction.client.commands.get(interaction.commandName);
 
     try {
@@ -46,5 +46,4 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-registerCommands();
 client.login(process.env.DISCORD_BOT_TOKEN);
