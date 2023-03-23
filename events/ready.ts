4ac54f48188ddc036391da7bef51ca4a3ccc46b0
Evan Tung <dragonejt@gmail.com>
2023-03-22 23:46:06 -0700

    change client type in ready

--- events/ready.ts
+++ events/ready.ts
@@ -1,5 +1,5 @@
-import { Client } from "discord.js";
-export default async function ready(client: Client) {
+import { SibylDiscordClient } from "../clients/discord.js";
+export default async function ready(client: SibylDiscordClient) {
     console.log(`Logged in as ${client.user!.tag}!`);
     await client.guilds.fetch();
     client.user!.setPresence({ activities: [{ name: `${client.guilds.cache.size} Servers`, type: 3 }] });
