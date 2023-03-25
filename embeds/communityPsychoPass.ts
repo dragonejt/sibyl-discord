0b19e544fe1c14b5a3496a99d1d593d9024b031a
Evan Tung <dragonejt@gmail.com>
2023-03-24 21:06:36 -0700

    add embeds for psychoPass

---
+++ embeds/communityPsychoPass.ts
@@ -0,0 +1,22 @@
+import { User, Client, Guild } from "discord.js";
+import { EmbedBuilder } from "@discordjs/builders";
+import { CommunityPsychoPass } from "../clients/backend/psychopass/communityPsychoPasses.js";
+
+export default async function commnityPsychoPassEmbed(psychoPass: CommunityPsychoPass, client: Client, requester: User, server: Guild) {
+    return new EmbedBuilder()
+        .setTitle(`Psycho-Pass of Server: ${server.name}`)
+        .setAuthor({ name: "Sibyl System", iconURL: client.user?.avatarURL()! })
+        .setDescription(`Requested by ${requester.tag}`)
+        .setThumbnail(server.iconURL())
+        .addFields({ name: "Toxicity", value: psychoPass.area_stress_level.toxicity.toString() })
+        .addFields({ name: '\u200B', value: '\u200B' })
+        .addFields(
+            { name: "Severe Toxicity", value: psychoPass.area_stress_level.severe_toxicity.toString(), inline: true },
+            { name: "Identity Attack", value: psychoPass.area_stress_level.identity_attack.toString(), inline: true },
+            { name: "Insult", value: psychoPass.area_stress_level.insult.toString(), inline: true },
+            { name: '\u200B', value: '\u200B' },
+            { name: "Threat", value: psychoPass.area_stress_level.threat.toString(), inline: true },
+            { name: "Profanity", value: psychoPass.area_stress_level.profanity.toString(), inline: true },
+            { name: "Sexually Explicit", value: psychoPass.area_stress_level.sexually_explicit.toString(), inline: true },
+        )
+}
