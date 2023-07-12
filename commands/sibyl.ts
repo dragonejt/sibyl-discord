792eaf3d7ec86e926031bffdbc3158dc95274c7e
Evan Tung <dragonejt@gmail.com>
2023-07-12 16:53:34 -0700

    Add new role/channel to role/channel change

--- commands/sibyl.ts
+++ commands/sibyl.ts
@@ -13,35 +13,28 @@ const data = new SlashCommandBuilder()
 
 async function execute(interaction: ChatInputCommandInteraction) {
     await interaction.reply(`Sibyl Pong! Response Time: ${Date.now() - interaction.createdTimestamp}ms`);
-    if (interaction.options.get("log_channel")) {
-        if (!interaction.memberPermissions?.has(PermissionFlagsBits.Administrator)) {
-            interaction.followUp({
-                content: "You Do Not Have Permissions to Configure Notification Settings. You Must Have the Administrator Permission.",
-                ephemeral: true
-            });
-        } else {
+    if (interaction.memberPermissions?.has(PermissionFlagsBits.Administrator)) {
+        if (interaction.options.get("log_channel")) {
             const data = {
                 communityID: interaction.guildId,
                 discord_log_channel: interaction.options.get("log_channel")!.value!.toString()
             };
             communities.update(data);
-            interaction.followUp("Log Channel Has Been Updated.");
+            interaction.followUp(`Log Channel has been updated to <#${(await communities.read(interaction.guildId!))?.discord_log_channel}>`);
         }
-    }
-    if (interaction.options.get("notify_role")) {
-        if (!interaction.memberPermissions?.has(PermissionFlagsBits.Administrator)) {
-            interaction.followUp({
-                content: "You Do Not Have Permissions to Configure Notification Settings. You Must Have the Administrator Permission.",
-                ephemeral: true
-            });
-        } else {
+        if (interaction.options.get("notify_role")) {
             const data = {
                 communityID: interaction.guildId,
                 discord_notify_target: interaction.options.get("notify_role")!.value!.toString()
             };
             communities.update(data);
-            interaction.followUp("Notification Target Has Been Updated.");
+            interaction.followUp(`Notification Target has been updated to <@&${(await communities.read(interaction.guildId!))?.discord_notify_target}>`);
         }
+    } else if (interaction.options.get("log_channel") || interaction.options.get("notify_role")) {
+        interaction.followUp({
+            content: "You Do Not Have Permissions to Configure Notification Settings. You Must Have the Administrator Permission.",
+            ephemeral: true
+        });
     }
 }
 
