298bc05e4d17f0dabac213dba3668c4e01bad474
Evan Tung <dragonejt@gmail.com>
2023-10-15 10:45:49 -0400

    add const assertions

--- clients/constants.ts
+++ clients/constants.ts
@@ -1,5 +1,5 @@
 export const ATTRIBUTES = ["toxicity", "severe_toxicity", "identity_attack", "insult", "threat", "profanity", "sexually_explicit"];
-export const ACTIONS = ["NOTIFY", "REMOVE", "MUTE", "KICK", "BAN"];
+export const ACTIONS = ["NOTIFY", "REMOVE", "MUTE", "KICK", "BAN"] as const;
 export const DEFAULT_MUTE_PERIOD = 60 * 60 * 1000;
 
 export const ATTR_PRETTY = {
@@ -12,7 +12,7 @@ export const ATTR_PRETTY = {
     threat: "Threat",
     profanity: "Profanity",
     sexually_explicit: "Sexually Explicit"
-};
+} as const;
 
 export const ACTION_PRETTY = {
     NOTIFY: "Notify",
@@ -20,7 +20,7 @@ export const ACTION_PRETTY = {
     MUTE: "Mute",
     KICK: "Kick",
     BAN: "Ban"
-};
+} as const;
 
 export function buildStringChoice(choiceName: string, choiceValue: string) {
     return {
