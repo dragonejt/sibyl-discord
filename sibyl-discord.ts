f514a1934fc9c5f0734bfee8536f91dd46c8255a
Evan Tung <dragonejt@gmail.com>
2024-04-21 18:42:40 -0400

    fix sentry sourcemaps

--- sibyl-discord.ts
+++ sibyl-discord.ts
@@ -25,7 +25,8 @@ init({
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
     // Set sampling rate for profiling - this is relative to tracesSampleRate
     profilesSampleRate: 1.0,
-    enableTracing: true
+    enableTracing: true,
+    environment: process.env.NODE_ENV
 });
 
 const client = new SibylDiscordClient({
